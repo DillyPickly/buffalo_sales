e6a77968b90b3f855d3035c3c6f7a83ca2b216ed
Dylan <dylan.sabuda@gmail.com>
2021-03-24 03:50:03 -0400

    adding server command

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup bokeh serve ../buffalo_sales --allow-websocket-origin=localhost:1313
+nohup bokeh serve buffalo_sales --port 5100 --log-level=info --allow-websocket-origin='dylansabuda.com' &
